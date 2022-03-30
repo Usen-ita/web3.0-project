bcb986f2d99c04ecf4b7e84e9ccb196e1257f050
Usen <83946606+Usen-ita@users.noreply.github.com>
2022-03-30 09:50:45 +0100

    First web3 update

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Navbar, Footer, Loader, Service, Transactions, Welcome} from './components'
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Service/>
+      <Transactions/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
